81bb324d0f044d9ee00c8570cb9a65c818373bfc
Ryan <ryology231@gmail.com>
2023-06-03 13:56:22 -0700

    documentation, delayed transitions
    Paradigm for triggering transitions changed from immediately
    on page load to specifically calling transitionIn(). Can easily be
    done during window load; if we don't want to play an animation
    right away we also have the option to call it later.

--- source/HomePage/HomePage.js
+++ source/HomePage/HomePage.js
@@ -10,47 +10,16 @@ document.addEventListener("click", function (event) {
   }
 
   if (elWithLink) {
-    event.preventDefault();
-    let transitioner = new Transitioner();
-    console.log(new URL(elWithLink.href).href);
-    transitioner.seturl(new URL(elWithLink.href).href);
-    transitioner.fadeBlackFadeIn(5);
-    return;
+    // TODO: intercept clicks and play transCanv animation first before exiting
+    let transCanv = document.querySelector(`canvas[is="transition-canvas]"`);
+    if (transCanv === undefined) {
+      return;
+    }
   }
 });
 
-class Transitioner {
-  constructor() {
-    this.url = window.location.href;
-  }
-
-  seturl(url) {
-    this.url = url;
-  }
-
-  /**
-   * Fetches a page. Useful for obtaining page data for custom usage rather than
-   * relying on the default behavior of loading a new page.
-   * @date 5/31/2023 - 1:51:15 AM
-   *
-   * @async
-   * @param {string} url
-   * @returns {string}
-   */
-  async loadtext(url) {
-    return (await fetch(url, { method: "GET" })).text();
-  }
-
-  async fadeBlackFadeIn(speed) {
-    // history.pushState(null, null, this.url);
-    const pagedata = await this.loadtext(this.url);
-    // const response = await fetch(this.url, {method: "GET"});
-    // const blob = await response.blob();
-    // const bruh = URL.createObjectURL(blob);
-    let wrapper = document.createElement('div');
-    wrapper.innerHTML = pagedata;
-
-    let newcontent = wrapper.querySelector('main');
-    document.querySelector('main').innerHTML = newcontent.innerHTML;
-  }
-}
+window.addEventListener("load", function () {
+  let transCanv = document.querySelector(`canvas[is="transition-canvas"]`);
+  console.log(transCanv);
+  transCanv.transitionIn();
+});
--- source/transition-canvas/transition-canvas.js
+++ source/transition-canvas/transition-canvas.js
@@ -1,4 +1,13 @@
+/**
+ * An extension of the canvas with extra functionality for transitions between pages.
+ * @date 6/3/2023 - 12:07:15 PM
+ *
+ * @class TransitionCanvas
+ * @typedef {TransitionCanvas}
+ * @extends {HTMLCanvasElement}
+ */
 class TransitionCanvas extends HTMLCanvasElement {
+  static observedattributes = ["sfx"];
   constructor() {
     super();
 
@@ -10,25 +19,65 @@ class TransitionCanvas extends HTMLCanvasElement {
     this.style.pointerEvents = "none";
     // some arbitrarily high index. If we need to we can place objects over the canvas.
     this.style.zIndex = "100";
+
+    // audio
+    this.inSfx = new Audio();
   }
 
+  /**
+   * Upon connection, check the duration of the transition, an important parameter for the animations.
+   * Also, add an optional sound effect upon transition (WIP).
+   * @date 6/3/2023 - 12:06:00 PM
+   */
   connectedCallback() {
+    this.after(this.inSfx);
+    // sound effect
+    if (this.hasAttribute("sfx")) {
+      this.inSfx.src = this.getAttribute("sfx");
+    }
+
+    // Transition
     this.duration = this.hasAttribute("duration")
       ? this.getAttribute("duration")
       : 500;
-    this.transitionIn();
+    this.fadeIntoViewAnim(true);
   }
 
+  /**
+   * If the sfx attribute were to be changed viaa script, this lifecycle callback
+   * will change the sfx of the audio element linked to this transition-canvas.
+   * Convenient for development using this canvas.
+   * @date 6/3/2023 - 1:23:09 PM
+   *
+   * @param {*} attribute
+   * @param {*} previousValue
+   * @param {*} currentValue
+   */
+  attributeChangedCallback(attribute, previousValue, currentValue) {
+    if ((attribute = "sfx")) {
+      this.inSfx.src = currentValue;
+    }
+  }
+
+  /**
+   * Initiates the transition in animation. This plays immediately upon the canvas being connected
+   * to the DOM.
+   * @date 6/3/2023 - 12:07:54 PM
+   */
   transitionIn() {
     // TODO
     const pen = this.getContext("2d");
     pen.clearRect(0, 0, this.width, this.height);
     pen.fillStyle = "black";
     pen.fillRect(0, 0, 100, 100);
-    this.fadeIntoViewAnim();
+    this.fadeIntoViewAnim(false);
   }
 
-  fadeIntoViewAnim() {
+  /**
+   * Definition for the animation of starting from a black screen and slowly lightening up to reveal the rest of the scene.
+   * @date 6/3/2023 - 12:08:56 PM
+   */
+  fadeIntoViewAnim(firstFrameOnly) {
     let start, previousTimestamp;
     let duration = this.duration;
     let canvas = this;
@@ -49,7 +98,7 @@ class TransitionCanvas extends HTMLCanvasElement {
         pen.fillRect(0, 0, width, height);
       }
 
-      if (elapsed < duration) {
+      if (!firstFrameOnly && elapsed < duration) {
         window.requestAnimationFrame(step);
       }
     }
