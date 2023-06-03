2f713414126804c8e37385d1e72539d4c9038d3d
Ryan <ryology231@gmail.com>
2023-06-03 04:20:33 -0700

    Added a transition-canvas custom element
    The transition-canvas will be the basis for playing animations between
    the various pages of the app. Currently, there is only one animation,
    which is where the app fades into view. However, the component
    was designed to be modular enough to give developers an easy time
    developing new transitions.

---
+++ source/transition-canvas/transition-canvas.js
@@ -0,0 +1,66 @@
+class TransitionCanvas extends HTMLCanvasElement {
+  constructor() {
+    super();
+
+    this.style.width = "100%";
+    this.style.height = "100%";
+    this.style.position = "fixed";
+    this.style.left = 0;
+    this.style.top = 0;
+    // some arbitrarily high index. However, it shouldn't have
+    this.style.zIndex = "100";
+  }
+
+  connectedCallback() {
+    this.duration = this.hasAttribute("duration")
+      ? this.getAttribute("duration")
+      : 500;
+    this.transitionIn();
+  }
+
+  transitionIn() {
+    // TODO
+    const pen = this.getContext("2d");
+    pen.clearRect(0, 0, this.width, this.height);
+    pen.fillStyle = "black";
+    pen.fillRect(0, 0, 100, 100);
+    this.fadeIntoViewAnim();
+  }
+
+  fadeIntoViewAnim() {
+    let start, previousTimestamp;
+    let duration = this.duration;
+    let canvas = this;
+    let width = this.width;
+    let height = this.height;
+    function step(timestamp) {
+      if (start === undefined) {
+        start = timestamp;
+      }
+
+      const elapsed = timestamp - start;
+      if (previousTimestamp != timestamp) {
+        // ANIMATE!
+        const opacity = 1 - elapsed / duration;
+        console.log("opactiy: " + opacity);
+        let pen = canvas.getContext("2d");
+        pen.clearRect(0, 0, width, height);
+        pen.fillStyle = `rgba(0, 0, 0, ${opacity})`;
+        pen.fillRect(0, 0, width, height);
+      }
+
+      if (elapsed < duration) {
+        window.requestAnimationFrame(step);
+      }
+    }
+    window.requestAnimationFrame(step);
+  }
+
+  transitionOut() {
+    // TODO
+  }
+}
+
+customElements.define("transition-canvas", TransitionCanvas, {
+  extends: "canvas",
+});
