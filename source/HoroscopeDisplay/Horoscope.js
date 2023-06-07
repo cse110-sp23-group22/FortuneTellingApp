e4434fb7e946732862ebaaeded772a62e3f640c7
jtanurdjaja <97708996+jtanurdjaja@users.noreply.github.com>
2023-06-07 00:15:10 -0700

    Updated JSDocs, cleaned up comments

--- source/HoroscopeDisplay/Horoscope.js
+++ source/HoroscopeDisplay/Horoscope.js
@@ -1,62 +1,74 @@
-var birthday; //birthday input, got from local storage
-var horoscopeArray; //array including the horoscope responses
-var homeButton; //button used to go back to the home page
+/**
+ * This file contains the functions related to the horoscope feature
+ * @namespace Horoscope
+ */
 
-//init function on load of the page
+var birthday; // birthday input, got from local storage
+var horoscopeArray; // array including the horoscope responses
+var homeButton; // button used to go back to the home page
+
+// init function on load of the page
 window.addEventListener("load", init);
 
 /**
- * @description init function used to fetch horoscope.json file and set up the home page
+ * @memberOf Horoscope
+ * @async
+ * @function init
+ * @description An init function used to fetch horoscope.json file and set up the home page
  * event listener behavior. Exit code 1 if successful, 0 if not.
- * @tutorial horoscope-tutorial
- * @class Horoscope
  * @author Nikan, Bill, Jennifer
  * @throws Fetch API failure errors
  */
 async function init() {
   try {
-    //fetch the horoscope json file and convert to an array
-    //containing the responses
+    // fetch the horoscope json file and convert to an array
+    // containing the responses
     horoscopeArray = await fetch("./horoscope.JSON");
     horoscopeArray = await horoscopeArray.json();
     horoscopeArray = horoscopeArray["data"];
 
-    //get homeButton and set up event listener
+    // get homeButton and set up event listener
     homeButton = document.getElementById("homeButton");
     homeButton.addEventListener("click", goHome);
 
-    //Run Timer
+    // Run Timer
     timer();
-    //output the horoscope based on the birthday of the user
+    // output the horoscope based on the birthday of the user
     outputHoroscope();
-    //exit code
+    // exit code
     return 1;
   } catch (error) {
-    //catch errors with exit code 0
+    // catch errors with exit code 0
     console.error(error);
     return 0;
   }
 }
 
 /**
- * Timer Event -> ! This function Triggers ALL TIME BASED EVENTS
+ * @memberOf Horoscope
+ * @async
+ * @function timer
+ * @description A timer event that triggers all time based events
+ * @author Nikan
  */
 async function timer() {
-  //TODO: Start the ticking event (SOUND)
+  // TODO: Start the ticking event (SOUND)
   concentricGradient();
-  //After 10 seconds start blood red screen
+  // After 10 seconds start blood red screen
   await new Promise((resolve) => setTimeout(resolve, 10000));
   pulseRedOverlay(1000);
-  //Start shaking after 10 seconds
+  // Start shaking after 10 seconds
   await new Promise((resolve) => setTimeout(resolve, 10000));
   shake(1, 1);
 }
 
 /**
- * Shake Element shakes the body by translating X and rotating
- * @param {integer} shakeXAmount
- * @param {integer} RotateAmount
- * Both of these parameters determine the resulting shake look
+ * @memberOf Horoscope
+ * @function shake
+ * @description Shakes the body by translating X and rotating
+ * @param {integer} shakeXAmount - The amount of times the shaking motion should occur
+ * @param {integer} RotateAmount - The amount the shake should rotate
+ * @author Nikan
  */
 function shake(shakeXAmount, RotateAmount) {
   let body = document.getElementsByClassName("page-group")[0];
@@ -73,7 +85,9 @@ function shake(shakeXAmount, RotateAmount) {
 }
 
 /**
- * Slowly fades in closer and closer
+ * @memberOf Horoscope
+ * @function concentricGradient
+ * @description Slowly fades in closer and closer
  */
 function concentricGradient() {
   let gradient = document.getElementById("gradient");
@@ -87,46 +101,43 @@ function concentricGradient() {
 }
 
 /**
- * Pulses the overlay div. This is supposed to be a blood screen
- * @param {integer} transitionTime
- * How long does the transition take. Also determines time between each pulse
+ * @memberOf Horoscope
+ * @function pulseRedOverlay
+ * @description Responsible for pulses on overlay div for the blood screen affect
+ * @param {integer} transitionTime - The length of transitions and time between each pulse
  */
 function pulseRedOverlay(transitionTime) {
   let overlay = document.getElementsByClassName("overlay")[0];
   let delay = transitionTime;
-  let redColor = 255; //255 is the greatest saturation of red
+  let redColor = 255; // 255 is the greatest saturation of red
   let interval = setInterval(() => {
-    overlay.style.transition = `opacity ${delay / 1000}`; //Changes transition delay (From ms -> s)
+    overlay.style.transition = `opacity ${delay / 1000}`; // Changes transition delay (From ms -> s)
     overlay.style.background = `rgb(${redColor},0,0)`;
     redColor -= 50; //* Change this to make it less or more darker each delay
-    //delay *= 0.9; //Added delay in case you want to pulse faster
+    // delay *= 0.9; //Added delay in case you want to pulse faster
     overlay.classList.toggle("shown");
   }, delay * 2);
 }
-/*
- *expamle test function that returns the input
- */
-function exampleTest(num) {
-  return num;
-}
+
 /**
- * @description Based on the input name and birthday, a horoscope reading will be outputted to the user
- * @tutorial horoscope-tutorial
+ * @memberOf Horoscope
+ * @function outputHoroscope
+ * @description A horoscope reading is outputted based on the input name and birthday
  * @author Nikan, Bill, Jennifer
  */
 function outputHoroscope() {
-  //get birthday and name from local storage
+  // get birthday and name from local storage
   let customerBirthday = localStorage.getItem("Birthday");
   let customerName = localStorage.getItem("UserName");
 
-  //set up birthdate in correct format and get numerical month
-  //note: added time at noon, since date object would change
-  //to EST if not specified and change the date by one
+  // set up birthdate in correct format and get numerical month
+  // note: added time at noon, since date object would change
+  // to EST if not specified and change the date by one
   birthday = new Date(`${customerBirthday}T12:00:00`);
   let birthdayMonth = birthday.getMonth();
   let birthdayDay = birthday.getDate();
 
-  //the correct index corresponding to the horscope sign of the birthday
+  // the correct index corresponding to the horscope sign of the birthday
   let horoscopeSignIndex = adjustIndexForHorroscope(birthdayDay, birthdayMonth);
   console.log(horoscopeSignIndex);
 
@@ -134,17 +145,17 @@ function outputHoroscope() {
   let todayDate = new Date();
   let todayDay = todayDate.getDate();
 
-  //randomize the responese of the horroscope by using the date of today to offset
-  //the resopnse of the array
+  // randomize the responese of the horroscope by using the date of today to offset
+  // the resopnse of the array
   let hashValue = (todayDay + horoscopeSignIndex) % 12;
 
-  //get output elements from the horoscope.html file
+  // get output elements from the horoscope.html file
   let horoscopeOutput = document.getElementById("horoscopeOutput");
   let nameOutput = document.getElementById("fname");
   let birthdayOutput = document.getElementById("birthday");
   let zodiacOutput = document.getElementById("zodiacSign");
 
-  //display the output in the correct element
+  // display the output in the correct element
   nameOutput.innerHTML = customerName;
   birthdayOutput.innerHTML = customerBirthday;
   zodiacOutput.innerHTML = horoscopeArray[horoscopeSignIndex]["sign"];
@@ -152,8 +163,9 @@ function outputHoroscope() {
 }
 
 /**
- * @description changes the location of the window to the home page
- * @tutorial horoscope-tutorial
+ * @memberOf Horoscope
+ * @function goHome
+ * @description Changes the location of the window to the home page
  * @author Nikan, Bill, Jennifer
  */
 function goHome() {
@@ -161,34 +173,33 @@ function goHome() {
 }
 
 /**
+ * @memberOf Horoscope
+ * @function adjustIndexForHorroscope
  * @description returns the index corresponding to the sign in the horoscope.JSON
  *                  file based on the input user birthday, using the hardcoded
- *                  date values.
- * @tutorial horoscope-tutorial
- * @param {int} day day of the user birthday
- * @param {int} month month of the user birthday
+ *                  date values. The following is how signs are selected:
+ *                  Aries (March 21 – April 19)
+ *                  Taurus (April 20 – May 20)
+ *                  Gemini (May 21 – June 20)
+ *                  Cancer (June 21 – July 22)
+ *                  Leo (July 23 – August 22)
+ *                  Virgo (August 23 – September 22)
+ *                  Libra (September 23 – October 22)
+ *                  Scorpio (October 23 – November 21)
+ *                  Sagittarius (November 22 – December 21)
+ *                  Capricorn (December 22 – January 19)
+ *                  Aquarius (January 20 – February 18)
+ *                  Pisces (February 19 – March 20)
+ * @param {int} day - day of the user birthday
+ * @param {int} month - month of the user birthday
  * @returns index of the zodiac sign
  * @author Nikan
  */
 function adjustIndexForHorroscope(day, month) {
-  /*
-    Aries (March 21 – April 19)
-    Taurus (April 20 – May 20)
-    Gemini (May 21 – June 20)
-    Cancer (June 21 – July 22)
-    Leo (July 23 – August 22)
-    Virgo (August 23 – September 22)
-    Libra (September 23 – October 22)
-    Scorpio (October 23 – November 21)
-    Sagittarius (November 22 – December 21)
-    Capricorn (December 22 – January 19)
-    Aquarius (January 20 – February 18)
-    Pisces (February 19 – March 20)
-    */
 
-  let newIndex; //the index corresponding to the sign in the horoscope.JSON file
+  let newIndex; // the index corresponding to the sign in the horoscope.JSON file
 
-  //switch statement with the hardcoded values of the days.
+  // switch statement with the hardcoded values of the days.
   switch (month) {
     case 0: // January
       switch (true) {
