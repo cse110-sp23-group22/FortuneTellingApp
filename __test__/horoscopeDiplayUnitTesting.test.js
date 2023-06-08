04f2964417ae54dd572c9130e4fb5984a9f12ee0
niknak1379 <nikanostovan@gmail.com>
2023-06-07 20:51:09 -0700

    Added some unit tests for the date function

---
+++ __test__/horoscopeDiplayUnitTesting.test.js
@@ -0,0 +1,19 @@
+/**
+ * @jest-environment jsdom
+ */
+const functions = require("../source/HoroscopeDisplay/horoscope.js");
+
+//checks bottom of the if statement
+test("tests the Index adjuster for the horoscope hardcoded values", () => {
+  expect(functions.adjustIndexForHorroscope(1, 0)).toBe(0);
+});
+
+//checks the last case when the dates loop back to jan
+test("tests the Index adjuster for the horoscope hardcoded values", () => {
+  expect(functions.adjustIndexForHorroscope(31, 11)).toBe(0);
+});
+
+//checks top of the if statement
+test("tests the Index adjuster for the horoscope hardcoded values", () => {
+  expect(functions.adjustIndexForHorroscope(18, 1)).toBe(1);
+});
