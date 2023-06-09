1747c2ad77e83915a0bf79812d9c723f62b9bea4
hjxhh030315 <jessica.jx.he@gmail.com>
2023-06-09 01:25:28 -0700

    edit the horoscope test file

--- __test__/HoroscopeDisplay.test.js
+++ __test__/HoroscopeDisplay.test.js
@@ -1,105 +1,106 @@
 const puppeteer = require("puppeteer");
 
 describe("My Puppeteer tests", () => {
-  let browser;
-  let page;
+    let browser;
+    let page;
 
-  beforeAll(async () => {
-    browser = await puppeteer.launch();
-    page = await browser.newPage();
-  });
-
-  afterAll(async () => {
-    await browser.close();
-  });
-
-  test("Tests if the contents of the local storage get displayed in the correcd tag", async () => {
-    await page.goto(
-      "https://cse110-sp23-group22.github.io/FortuneTellingApp/source/HoroscopeDisplay/Horoscope.html"
-    );
-    await page.evaluate(() => {
-      localStorage.setItem("Birthday", "02/02/2001");
-      localStorage.setItem("UserName", "Nikan");
-      console.log(localStorage.getItem("UserName"));
+    beforeAll(async() => {
+        browser = await puppeteer.launch();
+        page = await browser.newPage();
     });
-    //idk for some reason it doesnt recognize that localstorage was set so u have to reload the page a few times.
-    await page.goto(
-      "https://cse110-sp23-group22.github.io/FortuneTellingApp/source/HoroscopeDisplay/Horoscope.html"
-    );
-    await page.goto(
-      "https://cse110-sp23-group22.github.io/FortuneTellingApp/source/HoroscopeDisplay/Horoscope.html"
-    );
-    //await page.waitForNavigation();
 
-    console.log("checking output tags");
+    afterAll(async() => {
+        await browser.close();
+    });
 
-    let nameElement = await page.$("#fname");
-    let nameText = await (
-      await nameElement.getProperty("innerText")
-    ).jsonValue();
-    expect(nameText).toBe("Nikan");
+    test("Tests if the contents of the local storage get displayed in the correcd tag", async() => {
+        await page.goto(
+            "https://cse110-sp23-group22.github.io/FortuneTellingApp/source/HoroscopeDisplay/Horoscope.html"
+        );
+        await page.evaluate(() => {
+            localStorage.setItem("Birthday", "02/02/2001");
+            localStorage.setItem("UserName", "Nikan");
+            console.log(localStorage.getItem("UserName"));
+        });
+        //idk for some reason it doesnt recognize that localstorage was set so u have to reload the page a few times.
+        await page.goto(
+            "https://cse110-sp23-group22.github.io/FortuneTellingApp/source/HoroscopeDisplay/Horoscope.html"
+        );
+        await page.goto(
+            "https://cse110-sp23-group22.github.io/FortuneTellingApp/source/HoroscopeDisplay/Horoscope.html"
+        );
+        //await page.waitForNavigation();
 
-    let birthdayElement = await page.$("#birthday");
-    let birthdayText = await (
-      await birthdayElement.getProperty("innerText")
-    ).jsonValue();
-    expect(birthdayText).toBe("02/02/2001");
-  });
+        console.log("checking output tags");
 
-  test("Tests if the page will move on to the home page if the home button in horoscope is pushed", async () => {
-    await page.goto(
-      "https://cse110-sp23-group22.github.io/FortuneTellingApp/source/HoroscopeDisplay/Horoscope.html"
-    );
+        let nameElement = await page.$("#fname");
+        let nameText = await (
+            await nameElement.getProperty("innerText")
+        ).jsonValue();
+        expect(nameText).toBe("Nikan");
 
-    let oldUrl = page.url();
-    expect(oldUrl).toBe(
-      "https://cse110-sp23-group22.github.io/FortuneTellingApp/source/HoroscopeDisplay/Horoscope.html"
-    );
+        let birthdayElement = await page.$("#birthday");
+        let birthdayText = await (
+            await birthdayElement.getProperty("innerText")
+        ).jsonValue();
+        expect(birthdayText).toBe("02/02/2001");
+    });
 
-    let homeButton = await page.$("#homeButton");
-    await homeButton.click();
-    await page.waitForNavigation();
+    test("Tests if the page will move on to the home page if the home button in horoscope is pushed", async() => {
+        await page.goto(
+            "https://cse110-sp23-group22.github.io/FortuneTellingApp/source/HoroscopeDisplay/Horoscope.html"
+        );
 
-    let newUrl = page.url();
-    expect(newUrl).toBe(
-      "https://cse110-sp23-group22.github.io/FortuneTellingApp/index.html"
-    );
-  });
+        let oldUrl = page.url();
+        expect(oldUrl).toBe(
+            "https://cse110-sp23-group22.github.io/FortuneTellingApp/source/HoroscopeDisplay/Horoscope.html"
+        );
 
-  // test('Tests if output content is stored in localStorage', async () => {
+        let homeButton = await page.$("#homeButton");
+        await homeButton.click();
+        await page.waitForNavigation();
 
-  //   await page.goto('https://cse110-sp23-group22.github.io/FortuneTellingApp/source/Questionnaire/Questionnaire.html');
+        let newUrl = page.url();
+        expect(newUrl).toBe(
+            "https://cse110-sp23-group22.github.io/FortuneTellingApp/index.html"
+        );
+    });
 
-  //   await page.evaluate(() => {
-  //     document.getElementById('fname').value = 'Alex';
-  //   });
-  //   await page.evaluate(() => {
-  //     document.getElementById('birthday').value = '01/03/2000';
-  //   });
 
-  //   console.log('before');
-  //   //let button = await page.$('#exitButton');
-  //   //await button.click();
+    // test('Tests if output content is stored in localStorage', async () => {
 
-  //   //await page.reload();
-  //   await page.goto('https://cse110-sp23-group22.github.io/FortuneTellingApp/source/HoroscopeDisplay/horoscope.html');
+    //   await page.goto('https://cse110-sp23-group22.github.io/FortuneTellingApp/source/Questionnaire/Questionnaire.html');
 
-  //   console.log('after pressing button');
+    //   await page.evaluate(() => {
+    //     document.getElementById('fname').value = 'Alex';
+    //   });
+    //   await page.evaluate(() => {
+    //     document.getElementById('birthday').value = '01/03/2000';
+    //   });
 
-  //   //await button.click();
-  //   //await page.waitForNavigation();
+    //   console.log('before');
+    //   //let button = await page.$('#exitButton');
+    //   //await button.click();
 
-  //   let birthday = await page.evaluate(() => {
-  //     return localStorage.getItem('Birthday');
-  //   });
+    //   //await page.reload();
+    //   await page.goto('https://cse110-sp23-group22.github.io/FortuneTellingApp/source/HoroscopeDisplay/horoscope.html');
 
-  //   let username = await page.evaluate(() => {
-  //     return localStorage.getItem('Username');
-  //   });
+    //   console.log('after pressing button');
 
-  //   expect(username).toBe('Alex');
-  //   expect(birthday).toBe('01/03/2000');
-  // });
+    //   //await button.click();
+    //   //await page.waitForNavigation();
+
+    //   let birthday = await page.evaluate(() => {
+    //     return localStorage.getItem('Birthday');
+    //   });
+
+    //   let username = await page.evaluate(() => {
+    //     return localStorage.getItem('Username');
+    //   });
+
+    //   expect(username).toBe('Alex');
+    //   expect(birthday).toBe('01/03/2000');
+    // });
 });
 
 /*
