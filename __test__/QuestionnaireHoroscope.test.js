2e7d66493a52cc7ced3e6840283fdaa1a9775484
shuxin <vickyshuaaa@gmail.com>
2023-06-10 13:15:23 -0700

    add unit tests to HoroscopeQuestionnaire

--- __test__/QuestionnaireHoroscope.test.js
+++ __test__/QuestionnaireHoroscope.test.js
@@ -61,4 +61,64 @@ describe("questionnaire", () => {
     // Check that birthday value was set
     expect(birthdayInput.value).toBe("2000-01-01");
   });
+
+  test('parseNumbers function should return string without special characters', () => {
+    const input = ',./abc';
+    const output = questionnaire.parseNumbers(input);
+    expect(output).toBe('abc');
+  });
+
+  test('parseNumbers function should return empty string when there is only space', () => {
+    const input = '   ';
+    const output = questionnaire.parseNumbers(input);
+    expect(output).toBe('');
+  });
+  
+  test('showContent should append content from the correct template to the DOM', () => {
+    document.body.innerHTML = `<template><div id="template-content"></div></template><div id="questionnaire"></div>`;
+    questionnaire.showContent(0);
+    expect(document.getElementById('questionnaire').innerHTML).toContain('template-content');
+  });
+
+  test('initNameBirth should add an input event listener to nameInput', () => {
+    document.body.innerHTML = `<input id="fname" /> <input id="birthday" />`;
+    questionnaire.initNameBirth();
+    const inputElement = document.getElementById('fname');
+    const event = new Event('input');
+    inputElement.dispatchEvent(event);
+    expect(questionnaire.userName).toBe('');
+  });
+});
+
+describe('LocalStorage Tests:', () => {
+  // Create a mock localStorage
+  beforeEach(() => {
+    Object.defineProperty(window, 'localStorage', {
+      value: {
+        getItem: jest.fn(),
+        setItem: jest.fn(),
+        clear: jest.fn()
+      },
+      writable: true
+    });
+
+    window.onbeforeunload = jest.fn().mockImplementation(() => {
+      // Clear Local storage
+      localStorage.clear();
+      // Store data into local storage
+      localStorage.setItem('UserName', questionnaire.userName);
+      localStorage.setItem('Birthday', birthday);
+    });
+  });
+
+  test('window.onbeforeunload function should store data to localStorage', () => {
+    // Set the values of userName and birthday
+    questionnaire.userName = 'John';
+    birthday = '2000-01-01';
+    // Trigger the unload event
+    window.onbeforeunload();
+    // Check if localStorage.setItem was called correctly
+    expect(window.localStorage.setItem).toHaveBeenNthCalledWith(1, 'UserName', 'John');
+    expect(window.localStorage.setItem).toHaveBeenNthCalledWith(2, 'Birthday', '2000-01-01');
+  });
 });
