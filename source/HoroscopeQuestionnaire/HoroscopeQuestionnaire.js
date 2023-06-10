19046c5c2092ff77e20a9a98672099fede9c3d3c
ank010 <ank010@ucsd.edu>
2023-06-09 17:12:35 -0700

    Comment the Horoscope Form JS and CSS

--- source/HoroscopeQuestionnaire/HoroscopeQuestionnaire.js
+++ source/HoroscopeQuestionnaire/HoroscopeQuestionnaire.js
@@ -1,9 +1,9 @@
 /**
  * This file contains the functions for the Horoscope Questionnaire
- * @namespace Questionnaire
+ * @module HoroscopeQuestionnaire
  */
 
-let userName = "";
+let userName = ""; 
 let birthday = "";
 let templateNum = 0;
 let currentQuestion;
@@ -12,7 +12,7 @@ let totalNumQuestions = 1; //Counting starting from 0
 window.addEventListener("DOMContentLoaded", init);
 
 /**
- * @memberOf Questionnaire
+ * @memberOf HoroscopeQuestionnaire
  * @function init
  * @description An init function that starts up templates
  * @author Eric Chen, Jessica He, Chris Kim
@@ -22,27 +22,26 @@ export function init() {
 }
 
 /**
- * @memberOf Questionnaire
+ * @memberOf HoroscopeQuestionnaire
  * @function parseNumbers
  * @description A function that parses the input of all numbers
- * @param {string} string - The string of numbers to be parsed
+ * @param {string} string - string to be parsed (user's name)
  * @returns string with only valid letters
- * @author
+ * @author Eric, Jessica, Chris
  */
 export function parseNumbers(string) {
   return string.replace(/\d/g, "");
 }
 
 /**
- * @memberOf Questionnaire
+ * @memberOf HoroscopeQuestionnaire
  * @function showContent
  * @description Goes through all the questions in the questionnaire, displaying the questions one after another
  *                  based on when the user clicks the continue button.
  * @param templateNum - question set number
- * @author
+ * @author Eric, Jessica, Chris
  */
 export function showContent(templateNum) {
-  // Animation that plays on showing the content (Probably moving text and lights blowing out to new text)
   // Set content
   let temp = document.getElementsByTagName("template")[templateNum];
   currentQuestion = temp.content.cloneNode(true);
@@ -54,15 +53,15 @@ export function showContent(templateNum) {
       initNameBirth();
       break;
     default:
-    // By Default none of the other options mean anything rn
+    // By Default none of the other options mean anything
   }
 }
 
 /**
- * @memberOf Questionnaire
+ * @memberOf HoroscopeQuestionnaire
  * @function initNameBirth
  * @description Initalizes the name and birthday Question
- * @author
+ * @author Eric, Jessica, Chris
  */
 export function initNameBirth() {
   const nameInput = document.getElementById("fname");
@@ -84,7 +83,7 @@ export function initNameBirth() {
   });
 }
 /**
- * This Exit button. This function checks:
+ * Event listener for exit button. This function checks:
  * - If the user has inputted all required information
  * - Creates UI to prompt the user to confirm their selection
  * - Moves to the next page after all checks
@@ -95,9 +94,8 @@ window.addEventListener("DOMContentLoaded", () => {
     exitButton.addEventListener("click", async () => {
       // Check user has inputted all required information
       if (userName == "" || birthday == "") {
-        // Theorectically we have custom dialogs or something else that pops up to show that user hasn't inputted
+        // Alert pops up asking them to fill everything out
         alert("Please fill out required fields!");
-        // Idk play some spooky sound
         return;
       } else {
         //? Clear Local storage???
@@ -108,8 +106,6 @@ window.addEventListener("DOMContentLoaded", () => {
       }
       // Checks templateNum to see how far the user is to the end
       if (templateNum < totalNumQuestions) {
-        // TODO: Add cutscene
-        // console.log(noise.perlin3(time*0.05,0,0));
         let book = document.getElementById("book");
         book.classList.toggle("shakeElement");
         let overlay = document.getElementsByClassName("overlay")[0];
@@ -121,31 +117,17 @@ window.addEventListener("DOMContentLoaded", () => {
         overlay.style.transition = "opacity 1s";
         overlay.classList.toggle("shown");
         book.classList.toggle("shakeElement");
-        //? Currently not planning on users being able to go back.
         templateNum++;
         document.getElementById("question").remove();
         showContent(templateNum);
       } else {
-        // Moves to next page (Currently submit is used to call window.onbeforeunload)
-
+        // Moves to next page 
         window.location.href = "../HoroscopeDisplay/Horoscope.html";
         console.log("Exiting page");
       }
     });
   } else {
+    //The DOM hasn't loaded correctly
     console.log("exitButton is not found in the DOM");
   }
 });
-
-/**
- * On Exit Function -> Stores user data into local storage for further use
- */
-/*
-window.onbeforeunload = function () {
-  //? Clear Local storage???
-  localStorage.clear();
-  //Store data into local storage
-  localStorage.setItem("UserName", userName);
-  localStorage.setItem("Birthday", birthday);
-};
-*/
