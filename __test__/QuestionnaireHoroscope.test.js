59790c59c6ac18795c6040174be3f03efba3b942
Ryan <ryology231@gmail.com>
2023-06-11 14:04:36 -0700

    Fixed test suites not running
    Test suites failed due to the addition of home buttons in various
    parts of the page. We remedied this by adding those parts into the
    virtual DOMs in the tests.

--- __test__/QuestionnaireHoroscope.test.js
+++ __test__/QuestionnaireHoroscope.test.js
@@ -12,17 +12,29 @@ const { JSDOM } = require("jsdom");
 // A simple setup for JSDOM
 
 //const { window } = new JSDOM('<!doctype html><html><body><div id="exitButton"></div><template></template><div id="questionnaire"></div></body></html>');
-const { window } = new JSDOM(
-  '<!doctype html><html><body><div id="exitButton"></div><template></template><div id="questionnaire"></div></body></html>',
-  { url: "http://localhost/" }
+const dom = new JSDOM(
+  `<!DOCTYPE html>
+    <html>
+      <body>
+        <div id="exitButton"></div>
+        <button id="homeButton" class="back">Home</button>
+        <template></template>
+        <div id="questionnaire"></div>
+      </body>
+    </html>`
+  // { url: "http://localhost/" }
 );
 
-global.jsdomWindow = window;
-global.jsdomDocument = window.document;
-global.jsdomLocalStorage = window.localStorage;
+// global.jsdomWindow = window;
+// global.jsdomDocument = window.document;
+// global.jsdomLocalStorage = window.localStorage;
+
+global.document = dom.window.document;
+global.window = dom.window;
 
 // Then import your js file, assuming it's named myModule.js
-import * as questionnaire from "../source/HoroscopeQuestionnaire/HoroscopeQuestionnaire.js";
+// import * as questionnaire from "../source/HoroscopeQuestionnaire/HoroscopeQuestionnaire.js";
+const questionnaire = require("../source/HoroscopeQuestionnaire/HoroscopeQuestionnaire.js");
 
 describe("questionnaire", () => {
   beforeEach(() => {
@@ -63,7 +75,7 @@ describe("questionnaire", () => {
   });
 });
 
-describe('initNameBirth', () => {
+describe("initNameBirth", () => {
   beforeEach(() => {
     // Set up the initial state
     document.body.innerHTML = `
@@ -75,28 +87,28 @@ describe('initNameBirth', () => {
     questionnaire.initNameBirth();
   });
 
-  test('should update userName when input event occurs on the name input', () => {
-    const nameInput = document.getElementById('fname');
-    const name = 'John';
+  test("should update userName when input event occurs on the name input", () => {
+    const nameInput = document.getElementById("fname");
+    const name = "John";
 
     nameInput.value = name;
-    nameInput.dispatchEvent(new Event('input'));
+    nameInput.dispatchEvent(new Event("input"));
 
     expect(nameInput.value).toBe(name);
   });
 
-  test('should update birthday when change event occurs on the birthday input', () => {
-    const birthdayInput = document.getElementById('birthday');
-    const birthday = '2000-01-01';
+  test("should update birthday when change event occurs on the birthday input", () => {
+    const birthdayInput = document.getElementById("birthday");
+    const birthday = "2000-01-01";
 
     birthdayInput.value = birthday;
-    birthdayInput.dispatchEvent(new Event('change'));
+    birthdayInput.dispatchEvent(new Event("change"));
 
     expect(birthdayInput.value).toBe(birthday);
   });
 });
 
-describe('showContent', () => {
+describe("showContent", () => {
   let initNameBirthMock;
 
   beforeEach(() => {
@@ -120,27 +132,29 @@ describe('showContent', () => {
 
     // Mock template contents
     const templateContent0 = document.createDocumentFragment();
-    const template0 = document.getElementById('template0');
-    Object.defineProperty(template0, 'content', { value: templateContent0 });
+    const template0 = document.getElementById("template0");
+    Object.defineProperty(template0, "content", { value: templateContent0 });
 
     const templateContent1 = document.createDocumentFragment();
-    const template1 = document.getElementById('template1');
-    Object.defineProperty(template1, 'content', { value: templateContent1 });
+    const template1 = document.getElementById("template1");
+    Object.defineProperty(template1, "content", { value: templateContent1 });
   });
 
-  test('should append content from the correct template to the DOM', () => {
+  test("should append content from the correct template to the DOM", () => {
     questionnaire.showContent(0);
-    expect(document.getElementById('questionnaire').innerHTML).toContain('template-content');
+    expect(document.getElementById("questionnaire").innerHTML).toContain(
+      "template-content"
+    );
   });
 
-  test('should not call initNameBirth when templateNum is not 0', () => {
+  test("should not call initNameBirth when templateNum is not 0", () => {
     questionnaire.showContent(1);
     expect(initNameBirthMock).not.toHaveBeenCalled();
   });
 });
 
-describe('exitButton click event', () => {
-  let alertMock;
+describe("exitButton click event", () => {
+  // let alertMock;
   let exitButton;
 
   beforeEach(() => {
@@ -157,56 +171,55 @@ describe('exitButton click event', () => {
     `;
 
     // Set up initial state
-    questionnaire.userName = 'John';
-    questionnaire.birthday = '2000-01-01';
+    questionnaire.userName = "John";
+    questionnaire.birthday = "2000-01-01";
     questionnaire.templateNum = 0;
 
     // exitButton = document.getElementById('exitButton');
-    exitButton = global.document.getElementById('exitButton');
-    alertMock = jest.spyOn(global, 'alert').mockImplementation(() => {});
+    exitButton = global.document.getElementById("exitButton");
+    // alertMock = jest.spyOn(global, "alert").mockImplementation(() => {});
     // // window.alert = jest.fn();
     // jest.spyOn(console, 'log').mockImplementation(() => {});
   });
 
-  test('should display an alert if userName or birthday is empty', async () => {
+  test("should display an alert if userName or birthday is empty", async () => {
     // Clear userName and birthday
     questionnaire.userName = "";
     questionnaire.birthday = "";
 
-    exitButton.dispatchEvent(new global.window.Event('click'));
+    exitButton.dispatchEvent(new global.window.Event("click"));
     // await fireEvent.click(exitButton);
 
     expect(global.alert);
     // expect(alertMock).toHaveBeenCalledWith('Please fill out required fields!');
   });
 
-  test('should update localStorage and templateNum if userName and birthday are not empty', async () => {
+  test("should update localStorage and templateNum if userName and birthday are not empty", async () => {
     const localStorageMock = {
       clear: jest.fn(),
-      setItem: jest.fn()
+      setItem: jest.fn(),
     };
 
     // Set up localStorage mock
-    Object.defineProperty(window, 'localStorage', {
+    Object.defineProperty(window, "localStorage", {
       value: localStorageMock,
-      writable: true
+      writable: true,
     });
 
     // assuming these are global variables - set them before the test
-    questionnaire.userName = 'John';
-    questionnaire.birthday = '2000-01-01';
+    questionnaire.userName = "John";
+    questionnaire.birthday = "2000-01-01";
 
     const handleClick = async () => {
-        localStorage.clear();
-        localStorage.setItem('UserName', 'John');
-        localStorage.setItem('Birthday', '2000-01-01');
-        // ... change templateNum ...
+      localStorage.clear();
+      localStorage.setItem("UserName", "John");
+      localStorage.setItem("Birthday", "2000-01-01");
+      // ... change templateNum ...
     };
-    
-    exitButton.addEventListener('click', handleClick);
-    
-    // dispatch event and wait for any Promises to resolve
-    await exitButton.dispatchEvent(new Event('click'));
-  });
 
+    exitButton.addEventListener("click", handleClick);
+
+    // dispatch event and wait for any Promises to resolve
+    await exitButton.dispatchEvent(new Event("click"));
+  });
 });
